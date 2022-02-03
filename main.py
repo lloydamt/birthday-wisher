69b68774c4f09832e23739924ff2053c248e1699
Adawari <adawarithomas@gmail.com>
2022-02-03 01:05:44 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas
+
+# 1 - read birthdays
+# 2 - loop through birthdays and check if any date is equal to the present date
+# 3 - create an smtp connection
+# 4 - select random mail from available mails and open file to read its contents
+# 5 - replace letter name with recipient name
+
+
+
